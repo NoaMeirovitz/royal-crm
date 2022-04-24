fc73ceeea82a793c44e49c9bc4ac26e7b5fb21de
Noa Meirovitz <noameirovitz16@gmail.com>
2022-04-24 17:55:48 +0300

    products and orders

--- orders.js
+++ orders.js
@@ -1,7 +1,22 @@
 const orders = [];
 
 function addOrder() {
-  orders.push({});
+  const order = process.argv.slice(2);
+
+  if (!order || order.length === 0) {
+    throw "Error: order is empty!";
+  }
+
+  const orderId = Math.floor(Math.random() * 100000);
+
+  orders.push({
+    order: order,
+    id: orderId,
+  });
+
+  orders.forEach((order) => {
+    console.log(`ok. order: ${order.order} with temporary id: ${order.id}.`);
+  });
 }
 
 addOrder();
--- products.js
+++ products.js
@@ -1,7 +1,24 @@
 const products = [];
 
 function addProduct() {
-  orders.push({});
+  const product = process.argv.slice(2);
+
+  if (!product || product.length === 0) {
+    throw "Error: order is empty!";
+  }
+
+  const productId = Math.floor(Math.random() * 100000);
+
+  products.push({
+    product: product,
+    productId: productId,
+  });
+
+  products.forEach((product) => {
+    console.log(
+      `ok. product: ${product.product} with temporary id: ${product.productId}.`
+    );
+  });
 }
 
 addProduct();
