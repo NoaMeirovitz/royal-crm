d058aae91c2922a87e0dfce46d7d4afe87df5681
Noa Meirovitz <noameirovitz16@gmail.com>
2022-05-07 10:25:27 +0300

    class coplete1

--- customers.js
+++ customers.js
@@ -1,22 +1,53 @@
+const mysql = require("mysql2");
+const config = require("../config/dev");
+
+const pool = mysql.createPool({
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+});
+
 module.exports = {
-  list: [],
-
-  addCustomer: function () {
-    const name = process.argv.slice(2);
-
+  addCustomer: function (name, phone, email, countryId) {
     if (!name || name.length === 0) {
       throw "ERROR: name is empty";
     }
 
-    this.list.push({
-      name: name,
-      id: this.list.length,
+    pool.getConnection(function (connErr, connection) {
+      if (connErr) throw connErr; // not connected!
+
+      const sql =
+        "INSERT INTO customers(name, phone, email, country_id)" +
+        " VALUES(?,?,?,?);";
+
+      connection.query(
+        sql,
+        [name, phone, email, countryId],
+        function (sqlErr, result, fields) {
+          if (sqlErr) throw sqlErr;
+
+          console.log(fields);
+          console.log(result);
+        }
+      );
     });
   },
 
-  customersList: function () {
-    this.list.forEach((customer) => {
-      console.log(`ok. name: ${customer.name} was created.`);
+  customersList: function (req, res) {
+    pool.getConnection(function (connErr, connection) {
+      if (connErr) throw connErr; // not connected!
+
+      const sql = "SELECT * FROM customers";
+
+      connection.query(sql, function (sqlErr, result, fields) {
+        if (sqlErr) throw sqlErr;
+
+        res.send(result);
+      });
     });
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const customersModule = require("../controllers/customers");
+const productsModule = require("../controllers/products");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("okay bye Noa");
+  customersModule.addCustomer("Lola", "05233333", "hi@gmail.com", 1);
+  customersModule.customersList(req, res);
+});
+
+router.get("/products", function (req, res, next) {
+  productsModule.addProduct("Good Product", "A very good product", 50);
+  productsModule.productsList(req, res);
 });
 
 module.exports = router;
