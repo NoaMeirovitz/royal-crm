626bfce1c8d6d19720b3a3b6745d63d91673451c
Noa Meirovitz <noameirovitz16@gmail.com>
2022-06-05 11:00:03 +0300

    fill in 06.05

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+const mwAuth = require("../middleware/auth");
+const auth = require("../controllers/auth");
+const fileMgmt = require("../shared/fileMgmt");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
+/* authentication */
+router.get("/signin", function (req, res, next) {
+  const filePath = fileMgmt.getHtmlFilePath("login.html");
+  res.sendFile(filePath);
+});
+
+router.post("/login", auth.login);
+
+router.get("/logout", mwAuth, function (req, res, next) {
+  return res
+    .clearCookie("access_token")
+    .status(200)
+    .send("Successfully logged out.");
+});
+
+/* home page */
+router.get("/", mwAuth, function (req, res, next) {
   res.send(
     "this is the home page. use /customers/home /products/home or /orders/home."
   );
 });
 
-router.get("/chat", function (req, res, next) {
-  const filePath = path.join(__dirname, "../client", "chat.html");
+/* chat */
+router.get("/chat", mwAuth, function (req, res, next) {
+  const filePath = fileMgmt.getHtmlFilePath("chat.html");
   res.sendFile(filePath);
 });
 
