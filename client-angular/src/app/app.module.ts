8f78b1220010f40673d8570f1ee9e24a52f1f6fe
Noa Meirovitz <noameirovitz16@gmail.com>
2022-06-22 21:09:03 +0300

    sign up

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { CustomersComponent } from './customers/customers.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import { CustomersComponent } from './customers/customers.component';
     FooterComponent,
     HomeComponent,
     CustomersComponent,
+    ProductsComponent,
+    OrdersComponent,
+    SignupComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
