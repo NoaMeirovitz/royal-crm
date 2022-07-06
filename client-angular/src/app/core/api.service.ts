4c4fca430cf6d1176ba1e964d08de3ba48f7d905
Noa Meirovitz <noameirovitz16@gmail.com>
2022-07-06 18:17:15 +0300

    3 July

--- client-angular/src/app/core/api.service.ts
+++ client-angular/src/app/core/api.service.ts
@@ -14,6 +14,15 @@ export class ApiService {
     return this.http.get<Array<Customer>>(`${environment.serverUrl}/customers`);
   }
 
+  getSortedCustomers(
+    column: string,
+    direction: string
+  ): Observable<Array<Customer>> {
+    return this.http.get<Array<Customer>>(
+      `${environment.serverUrl}/customers?column=${column}&sort=${direction}`
+    );
+  }
+
   exportCustomers(): Observable<FilePath> {
     return this.http.get<FilePath>(`${environment.serverUrl}/customers/export`);
   }
